58b2e3caaccda8658227d499c8e0709b21e2e121
henriksson666 <128688845+henriksson666@users.noreply.github.com>
2025-01-18 00:33:59 -0300

    Refactor addResposta method to accept a list of responses for batch processing

--- projeto spring boot/demo/src/main/java/com/example/demo/controller/RespostasUsuarioQuestionarioController.java
+++ projeto spring boot/demo/src/main/java/com/example/demo/controller/RespostasUsuarioQuestionarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,13 +28,12 @@ public class RespostasUsuarioQuestionarioController {
         return ResponseEntity.ok(respostas);
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<Void> addResposta(
-            @RequestParam Long personId,
-            @RequestParam Long perguntaId,
-            @RequestParam Long respostaId) {
-        respostasUsuarioQuestionarioService.insertResposta(personId, perguntaId, respostaId);
+    @PostMapping("/enviarRespostasNivelamento")
+    public ResponseEntity<Void> addResposta(@RequestBody List<RespostasUsuarioQuestionario> respostasList) {
+        respostasList.forEach(resposta -> {
+            respostasUsuarioQuestionarioService.insertResposta(resposta.getIdPerson(), resposta.getIdPergunta(),
+                    resposta.getIdResposta());
+        });
         return ResponseEntity.ok().build();
     }
-
 }
