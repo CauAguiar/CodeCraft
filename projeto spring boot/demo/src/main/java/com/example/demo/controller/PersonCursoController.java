0cd3b41127832aa200fa1a058de5c88cc3b2051c
henriksson666 <128688845+henriksson666@users.noreply.github.com>
2025-01-18 16:26:44 -0300

    Enhance getPersonCurso method to return 404 status when PersonCurso is not found

--- projeto spring boot/demo/src/main/java/com/example/demo/controller/PersonCursoController.java
+++ projeto spring boot/demo/src/main/java/com/example/demo/controller/PersonCursoController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,14 @@ public class PersonCursoController {
     private PersonCursoService personCursoService;
 
     @GetMapping("/getByCursoIdAndPersonId")
-    public ResponseEntity<PersonCurso> getPersonCurso(@RequestParam(name = "personId") Long personId, @RequestParam(name = "cursoId") Long cursoId) {
+    public ResponseEntity<?> getPersonCurso(@RequestParam(name = "personId") Long personId, @RequestParam(name = "cursoId") Long cursoId) {
         System.out.println("Receive person id: " + personId + " and curso id: " + cursoId);
         PersonCurso personCurso = personCursoService.findByIdPersonAndCurso(personId, cursoId);
+        
+        if(personCurso == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("PersonCurso not found");
+        }
+        
         return ResponseEntity.ok(personCurso);
     }
 
